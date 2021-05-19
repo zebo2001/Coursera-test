8286da99f668062fd1b3db3e7b94bca69fb24642
Zebo Abdisamatova <77941770+zebo2001@users.noreply.github.com>
2021-05-18 19:08:18 -0700

    Add files via upload

---
+++ lesson1.java
@@ -0,0 +1,12 @@
+public class Lesson2 {
+	public static void main(String[] args) {
+		String s = "Hello";   
+		String bob = "Bob"
+		String dad = "dad"
+		System.out.println(s+bob+dad)
+		System.out.println("My number is" + num )
+		int num = 5;
+
+
+	}
+}
---
+++ lesson2.java
@@ -0,0 +1,36 @@
+public class  lesson2 {
+
+
+	public static void main(String[] args) {
+		for (int i =0; i<=10;  i++ )  {
+			System.out.println("Hello" + i) //i = i+1==i++
+		}
+	}
+}
+//until its true//for cycle
+
+//==========================================
+
+//if cycle
+
+
+
+public static void main(String[] args) {
+	int myInt = 15; 
+	if (myInt <20) {
+		System.out.println("true")
+	} else   (myInt >20) {
+		System.out.println("false")
+	} 
+
+}
+
+//========================================
+
+//class Scanner 
+
+public static void main(String[] args) {
+	String string = new String ( );
+	Scanner sc = new Scanner(); 
+
+}
