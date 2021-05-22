f58a80cba1be5a26431ecf92e1b305c36f1f9dad
Zebo Abdisamatova <77941770+zebo2001@users.noreply.github.com>
2021-05-21 22:26:53 -0700

    Add files via upload

---
+++ math notes.java
@@ -0,0 +1,38 @@
+public class Math {
+	public static void main(String[] args) {
+		String x = 1;
+		int result = String.parseString(x) + 2; ///converts string to int 
+
+
+		int math = Math.round(3.32F);
+
+		int result2 = (int)Math.ceil(1.1F);  //takes to close large
+
+		int result3 = Math.max(3,5);// takes the max number
+
+		double result4 = Math.rendom(100);
+
+		///////////////////////////////////////////
+
+		NumberFormat currency = NumberFormat.getCurrencyInstant
+		String result5 = currency.format (number: 1234567.891);
+		System.out.println(result5);
+
+		NumberFormat F = NumberFormat.getPercentInstant
+		String result5 = percent.format (number: 0.1);
+		System.out.println(result5);
+
+///////??//////////////////??///////////????///////??????/////////////
+
+		NumberFormat.getPercentInstant().format(number:0.1);
+
+/////////////////////////////////////////////////////////////
+
+		Scanner scanner = new Scanner
+
+
+
+
+
+	}
+}
